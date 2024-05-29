f2fe317fbff09902df13990aaf1457c2ba8ff0d8
atmiktumane <atmiktumane72@gmail.com>
2024-05-29 19:20:59 +0530

    updated Cart Page

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -3,21 +3,6 @@ import { CartCard } from "../components";
 import { useSelector } from "react-redux";
 
 export const CartPage = () => {
-  // const products = [
-  //   {
-  //     id: "1",
-  //     name: "Sony Wh-Ch510 Bluetooth Wireless",
-  //     price: 149,
-  //     image: "/images/1001.png",
-  //   },
-  //   {
-  //     id: "2",
-  //     name: "boAt Rockerz 450",
-  //     price: 49,
-  //     image: "/images/1002.png",
-  //   },
-  // ];
-
   const products = useSelector((state) => state.cartState.cartList);
   const total = useSelector((state) => state.cartState.total);
 
@@ -28,7 +13,7 @@ export const CartPage = () => {
   return (
     <section className="min-h-svh p-4">
       <h3 className="text-xl text-center">
-        CartList: {products.length} / TotalPrice: ${total}
+        CartItems: {products.length} / TotalPrice: ${total}
       </h3>
       <ul>{renderCartList}</ul>
     </section>
